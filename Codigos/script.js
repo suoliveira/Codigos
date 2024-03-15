72be6730cd080d997c4ec41d5219f2c98b8643ec
Suellen Oliveira <107051108+suoliveira@users.noreply.github.com>
2024-03-15 16:07:19 -0300

    Add files via upload

---
+++ Codigos/script.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.get("/proibido", (req, res) =>{
+    res.status(403).send("Acesso Proibido")
+});
+
+app.get("/notfound", (req, res) =>{
+    res.status(404).send("Não encontrado")
+});
+
+app.get("/nao", (req, res) =>{
+    res.status(401).send("Não autorizado")
+});
+
+app.get("/error" , (req, res) => {
+    res.status(500).send("Erro de servidor");
+});
+
+app.get("/", (req, res) =>{
+   res.status(200).send("Get feito");
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
